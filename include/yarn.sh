63e44a1596441d1e1b9706009ec22728b9d49c70
Naoaki Obiki <obiki.wk@gmail.com>
2019-02-13 15:20:57 +0900

    :bug: yarn PATH environment

--- include/yarn.sh
+++ include/yarn.sh
@@ -8,3 +8,5 @@ sudo apt install -y gcc g++ make
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install -y yarn
+
+echo 'PATH="$PATH":~/.yarn/bin/' >> ~/.bash_profile
