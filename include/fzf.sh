6ac26c09cbb3fc119af0b3cb4f07e917a8ec116f
Naoaki Obiki <obiki.wk@gmail.com>
2019-08-25 22:11:01 +0900

    :+1: fzf non-intaractive

--- include/fzf.sh
+++ include/fzf.sh
@@ -1,4 +1,4 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --completion --key-bindings --update-rc
 cp /usr/local/lib/dotfiles/.fzf.bash ~/.fzf.bash
 echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bash_profile
